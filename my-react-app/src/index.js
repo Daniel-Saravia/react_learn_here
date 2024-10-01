43c0ecadcaab6c1f04463246003ebc548bb8e8fd
Daniel Saravia <saraviad4444@gmail.com>
2024-10-01 07:23:49 -0700

    update

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Hello World!</h1>
+// Define the App component
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
 
+// Render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<App />);
